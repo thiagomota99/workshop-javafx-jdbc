82b94117d8205be49ec811402bbabe53e57ddc52
Thiago Mota <thiagomota99@outlook.com>
2022-10-03 19:17:45 -0300

    DepartmentList controller

---
+++ src/gui/DepartamentListController.java
@@ -0,0 +1,49 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Departamento;
+
+public class DepartamentListController implements Initializable {
+	
+	@FXML
+	private TableView<Departamento> tableViewDepartaments;
+	
+	@FXML
+	private TableColumn<Departamento, Integer> tableColumnId;
+	
+	@FXML
+	private TableColumn<Departamento, String> tableColumnName;
+	
+	@FXML
+	private Button btNovo;
+	
+	@FXML
+	public void onBtNovoAction() {
+		System.out.println("onBtNovoAction");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		Stage stage = (Stage) Main.getMainScene().getWindow(); //Pega referência da janela
+		tableViewDepartaments.prefHeightProperty().bind(stage.heightProperty()); //Define a altura da tabela conforme a altura da janela
+	}
+	
+	
+}
